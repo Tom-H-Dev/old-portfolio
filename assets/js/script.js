355aa6a3299a8576a30579fe23250c6ed236a1a1
Tom-H-Dev <tom.holewijn@protonmail.com>
2023-09-28 20:04:00 +0200

    temp pic of me fix 3 more projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -251,3 +251,20 @@ function toggleInfo(arrow) {
       arrow.textContent = "▼"; // Change arrow to down when collapsed
     }
   }
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
